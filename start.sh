b134e83f45788be38b03761e5dc5f19bcf2e80a6
mrgs83 <40406835+mrgs83@users.noreply.github.com>
2024-01-21 22:21:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 # Start the specified version of wicardd
 echo "Starting wicardd with options: $RUN_OPTS"
-/usr/local/bin/$RUN_OPTS -c /usr/local/bin/wicardd.conf &
+/usr/local/bin/$RUN_OPTS -c /config/wicardd.conf &
 
 # Infinite loop to keep the script alive
 while true; do
